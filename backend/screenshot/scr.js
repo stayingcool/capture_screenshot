3034549466f868f7956efaae29d29a9913ef4d4b
Nisar Ahmed <stayingcool@gmail.com>
2021-03-29 18:19:50 +1100

    Defect: Fix Screenshot File Name Time

--- backend/screenshot/scr.js
+++ backend/screenshot/scr.js
@@ -5,23 +5,28 @@ const TIME_INTERVAL = 1000 * 60; // Every 1 minute
 
 const getDate = () => {
   let today = new Date();
-  let currentDate = `${today.getDate()}-${
-    today.getMonth() + 1
-  }-${today.getFullYear()}`;
-  let currentTime = `${today.getHours()}h-${today.getMinutes()}m-${today.getSeconds()}s`;
-  today = `${currentDate}-${currentTime}`;
 
-  return today;
+  let dd = today.getDate(),
+    mm = today.getMonth() + 1,
+    yy = today.getFullYear();
+
+  let h = `${today.getHours()}`.padStart(2, "0"),
+    m = `${today.getMinutes()}`.padStart(2, "0"),
+    s = `${today.getSeconds()}`.padStart(2, "0");
+
+  let date = `${dd}-${mm}-${yy}`,
+    time = `${h}.${m}.${s}`;
+
+  return `${date}-${time}`;
 };
 
 const takeScreenShot = () => {
   let filename = `${getDate()}.png`;
-  screenshot({ filename: `../../frontend/img/${filename}` }).then(res=> {
-    console.log(res);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  screenshot({ filename: `../../frontend/img/${filename}` })
+    .then((res) => {})
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 // Main
